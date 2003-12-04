c715a6127a46b8090591972b5c4bb04dfabbca8b
rsc <devnull@localhost>
2003-12-04 19:18:28 +0000

    nan

---
+++ src/lib9/nan.c
@@ -0,0 +1,27 @@
+#include <u.h>
+#include <libc.h>
+#include "../libfmt/nan.h"
+
+double
+NaN(void)
+{
+	return __NaN();
+}
+
+double
+Inf(int sign)
+{
+	return __Inf(sign);
+}
+
+int
+isNaN(double x)
+{
+	return __isNaN(x);
+}
+
+int
+isInf(double x, int sign)
+{
+	return __isInf(x, sign);
+}
