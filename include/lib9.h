9e8eb69254ce76ff641839cb8c4afbaba4ab1e44
rsc <devnull@localhost>
2004-01-09 00:06:44 +0000

    add some more RF things for linux.

--- include/lib9.h
+++ include/lib9.h
@@ -674,15 +674,15 @@ extern	void		freenetconninfo(NetConnInfo*);
 
 enum
 {
-/*	RFNAMEG		= (1<<0), */
-/*	RFENVG		= (1<<1), */
+	RFNAMEG		= (1<<0), 
+	RFENVG		= (1<<1), 
 	RFFDG		= (1<<2),
 	RFNOTEG		= (1<<3),
 	RFPROC		= (1<<4),
 	RFMEM		= (1<<5),
 	RFNOWAIT	= (1<<6),
-/*	RFCNAMEG	= (1<<10), */
-/*	RFCENVG		= (1<<11), */
+	RFCNAMEG	= (1<<10), 
+	RFCENVG		= (1<<11), 
 	RFCFDG		= (1<<12),
 /*	RFREND		= (1<<13), */
 /*	RFNOMNT		= (1<<14) */
