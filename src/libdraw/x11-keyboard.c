cdd61ab0aee825e93688ae5d2cfb86f14baad858
rsc <devnull@localhost>
2004-04-21 17:52:46 +0000

    take out the debugging prints i just accidentally checked in.

--- src/libdraw/x11-keyboard.c
+++ src/libdraw/x11-keyboard.c
@@ -45,22 +45,16 @@ _ioproc(void *arg)
 	fd = XConnectionNumber(_x.kbdcon);
 	XSelectInput(_x.kbdcon, _x.drawable, KeyPressMask);
 	for(;;){
-		fprint(2, "x1 kc->c %p\n", kc->c);
 		while(XCheckWindowEvent(_x.kbdcon, _x.drawable, KeyPressMask, &xevent) == False){
-			fprint(2, "x2 kc->c %p\n", kc->c);
 			threadfdwait(fd, 'r');
-			fprint(2, "x3 kc->c %p\n", kc->c);
 		}
 		switch(xevent.type){
 		case KeyPress:
-			fprint(2, "x4 kc->c %p\n", kc->c);
 			i = _xtoplan9kbd(&xevent);
-			fprint(2, "x5 kc->c %p\n", kc->c);
 			if(i == -1)
 				continue;
 			r = i;
 			send(kc->c, &r);
-			fprint(2, "x6 kc->c %p\n", kc->c);
 			while((i=_xtoplan9kbd(nil)) >= 0){
 				r = i;
 				send(kc->c, &r);
