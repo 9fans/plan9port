f2479500fa3de375ffb51a479b8f62492a2910e0
rsc <devnull@localhost>
2004-05-20 18:17:35 +0000

    remove some debugging

--- src/libhttpd/hio.c
+++ src/libhttpd/hio.c
@@ -158,14 +158,11 @@ hreadbuf(Hio *h, void *vsave)
 			hh->pos += in;
 		}
 	}else if(in){
-fprint(2, "read %d from %d\n", in, h->fd);
 		if((in = read(h->fd, h->start + cpy, in)) < 0){
-fprint(2, "got error: %r\n");
 			h->state = Herr;
 			h->pos = h->stop;
 			return nil;
 		}
-fprint(2, "got %d\n", in);
 	}
 	if(in == 0)
 		h->state = Hend;
