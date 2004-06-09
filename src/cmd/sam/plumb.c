7e4a17cf3302c3cb4682d2f3d96a88ca951280e8
rsc <devnull@localhost>
2004-06-09 14:06:09 +0000

    use libplumb instead

--- src/cmd/sam/plumb.c
+++
@@ -1,9 +0,0 @@
-#include <u.h>
-#include "plumb.h"
-
-/* XXX - Can we do better than this? */
-char *cleanname(char *s) { return s; }
-char *plumbunpackattr(char *cbuf) {  return 0; }
-char *plumbpack(Plumbmsg *pm, int *i) { return 0; }
-int plumbfree(Plumbmsg *pm) { return 0; }
-
