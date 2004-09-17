282c88f9de897056f7a2461216dbfdb51ab655f3
rsc <devnull@localhost>
2004-09-17 22:09:31 +0000

    Bite the bullet.  Have to use different binaries on 2.4 and 2.6.

--- src/lib9/ffork-Linux.c
+++ src/lib9/ffork-Linux.c
@@ -1,32 +1,5 @@
-#define ffork ffork_clone
-#define getfforkid getfforkid_clone
-#include "ffork-Linux-clone.c"
-#undef ffork
-#undef getfforkid
-
-#define ffork ffork_pthread
-#define getfforkid getfforkid_pthread
+#ifdef __Linux26__
 #include "ffork-pthread.c"
-#undef ffork
-#undef getfforkid
-
-extern int _islinuxnptl(void);
-
-int
-ffork(int flags, void (*fn)(void*), void *arg)
-{
-	if(_islinuxnptl())
-		return ffork_pthread(flags, fn, arg);
-	else
-		return ffork_clone(flags, fn, arg);
-}
-
-int
-getfforkid(void)
-{
-	if(_islinuxnptl())
-		return getfforkid_pthread();
-	else
-		return getfforkid_clone();
-}
-
+#else
+#include "ffork-Linux-clone.c"
+#endif
