e63027eb68ff7252598003151d238dee8850ad34
rsc <devnull@localhost>
2004-10-22 17:08:13 +0000

    formatting

--- src/cmd/acidtypes/dat.h
+++ src/cmd/acidtypes/dat.h
@@ -20,12 +20,12 @@ enum
 struct Type
 {	/* Font Tab 4 */
 	uint	ty;			/* None, Struct, ... */
-	vlong lo;			/* for range */
-	char sue;
-	vlong hi;
+	vlong	lo;			/* for range */
+	char	sue;
+	vlong	hi;
 	uint	gen;
-	uint	n1;		/* type number (impl dependent) */
-	uint	n2;		/* another type number */
+	uint	n1;			/* type number (impl dependent) */
+	uint	n2;			/* another type number */
 	char	*name;		/* name of type */
 	char	*suename;	/* name of struct, union, enumeration */
 	uint	isunion;	/* is this Struct a union? */
@@ -33,13 +33,13 @@ struct Type
 	uint	xsizeof;	/* size of type */
 	Type	*sub;		/* subtype */
 	uint	n;			/* count for t, tname, val */
-	Type **t;			/* members of sue, params of function */
+	Type	**t;		/* members of sue, params of function */
 	char	**tname;	/* associated names */
 	long	*val;		/* associated offsets or values */
 	uint	didtypedef;	/* internal flag */
-	uint	didrange;		/* internal flag */
-	uint 	printed;		/* internal flag */
-	Type *equiv;		/* internal */
+	uint	didrange;	/* internal flag */
+	uint 	printed;	/* internal flag */
+	Type	*equiv;		/* internal */
 };
 
 struct TypeList
