6850febee1ff0eaa5e548209f211aaaa762e07e4
rsc <devnull@localhost>
2004-12-27 00:11:46 +0000

    porting changes

--- src/libbio/lib9.std.h
+++ src/libbio/lib9.std.h
@@ -1,12 +1,15 @@
+#include <utf.h>
 #include <fmt.h>
+
 #include <fcntl.h>
-#include	<string.h>
-#include	<unistd.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-#define OREAD O_RDONLY
-#define OWRITE O_WRONLY
-
-#include <utf.h>
+#define OREAD		O_RDONLY
+#define OWRITE	O_WRONLY
 
 #define nil ((void*)0)
+
+typedef long long vlong;
+typedef unsigned long long uvlong;
