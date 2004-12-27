3694b738b883dec5b93f11d4025541af5f713482
rsc <devnull@localhost>
2004-12-27 00:38:00 +0000

    handle pty correctly on linux

--- src/cmd/9term/Linux.c
+++ src/cmd/9term/Linux.c
@@ -1,1 +1,15 @@
+#define getpts not_using_this_getpts
 #include "bsdpty.c"
+#undef getpts
+
+int
+getpts(int fd[], char *slave)
+{
+	if(openpty(&fd[1], &fd[0], NULL, NULL, NULL) >= 0){
+		fchmod(fd[1], 0620);
+		strcpy(slave, ttyname(fd[0]));
+		return 0;
+	}
+	sysfatal("no ptys");
+	return 0;
+}
