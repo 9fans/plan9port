727facb60539f522635f0d8978435ce4680fecd6
rsc <devnull@localhost>
2004-12-28 19:18:33 +0000

    add bvprint

---
+++ src/libbio/bvprint.c
@@ -0,0 +1,39 @@
+#include "lib9.h"
+#include <bio.h>
+
+static int 
+fmtBflush(Fmt *f)
+{
+	Biobuf *bp;
+
+	bp = f->farg;
+	bp->ocount = (char*)f->to - (char*)f->stop;
+	if(Bflush(bp) < 0)
+		return 0;
+	f->stop = bp->ebuf;
+	f->to = (char*)f->stop + bp->ocount;
+	f->start = f->to;
+	return 1;
+}
+
+int
+Bvprint(Biobuf *bp, char *fmt, va_list arg)
+{
+	int n;
+	Fmt f;
+
+	f.runes = 0;
+	f.stop = bp->ebuf;
+	f.start = (char*)f.stop + bp->ocount;
+	f.to = f.start;
+	f.flush = fmtBflush;
+	f.farg = bp;
+	f.nfmt = 0;
+	f.args = arg;
+	n = dofmt(&f, fmt);
+	bp->ocount = (char*)f.to - (char*)f.stop;
+	return n;
+}
+
+
+	
