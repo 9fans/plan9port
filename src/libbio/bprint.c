f5ff4947dd510fa5b6ab6fc71ce2515a313402ce
rsc <devnull@localhost>
2004-12-28 03:41:17 +0000

    add va_start/va_end.

--- src/libbio/bprint.c
+++ src/libbio/bprint.c
@@ -10,7 +10,9 @@ Bprint(Biobuf *bp, char *fmt, ...)
 
 	if(Bfmtinit(&f, bp) < 0)
 		return -1;
+	va_start(args, fmt);
 	n = fmtprint(&f, fmt, args);
+	va_end(args);
 	if(n > 0 && Bfmtflush(&f) < 0)
 		return -1;
 	return n;
