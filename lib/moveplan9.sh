0070bb81c4729c1414a93534282d7840e83d88b0
rsc <devnull@localhost>
2005-01-07 23:11:02 +0000

    tweaks

--- lib/moveplan9.sh
+++ lib/moveplan9.sh
@@ -2,9 +2,9 @@
 
 p=`cleanname $PLAN9`
 cd $PLAN9
-echo '
+echo "
 	X ,s;/usr/local/plan9;$p;g
 	X/'/w
 	q
-' | sam -d `cat lib/moveplan9.files` man/man*/*.* # >/dev/null 2>&1
+" | sam -d `cat lib/moveplan9.files` man/man*/*.* >/dev/null 2>&1
 
