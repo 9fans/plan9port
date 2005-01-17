b04c42e9eb74c0206a0e21abbf8eb061f8eecea6
rsc <devnull@localhost>
2005-01-17 21:32:55 +0000

    nptl dependencies

---
+++ lib/linux-isnptl.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(void)
+{
+	ulong x;
+
+	x = (ulong)pthread_self();
+	if(x < 1024*1024)
+		exit(1);	/* NOT NPTL */
+	exit(0);
+}
