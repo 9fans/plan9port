b1cd1d55e1418f2ed8236d488f815d0108b99598
rsc <devnull@localhost>
2005-02-08 21:02:54 +0000

    use sysfatal

--- src/lib9p/mem.c
+++ src/lib9p/mem.c
@@ -10,10 +10,8 @@ emalloc9p(ulong sz)
 {
 	void *v;
 
-	if((v = malloc(sz)) == nil) {
-		fprint(2, "out of memory allocating %lud\n", sz);
-		exits("mem");
-	}
+	if((v = malloc(sz)) == nil)
+		sysfatal("out of memory allocating %lud", sz);
 	memset(v, 0, sz);
 	setmalloctag(v, getcallerpc(&sz));
 	return v;
@@ -24,10 +22,8 @@ erealloc9p(void *v, ulong sz)
 {
 	void *nv;
 
-	if((nv = realloc(v, sz)) == nil) {
-		fprint(2, "out of memory allocating %lud\n", sz);
-		exits("mem");
-	}
+	if((nv = realloc(v, sz)) == nil)
+		sysfatal("out of memory reallocating %lud", sz);
 	if(v == nil)
 		setmalloctag(nv, getcallerpc(&v));
 	setrealloctag(nv, getcallerpc(&v));
@@ -39,10 +35,8 @@ estrdup9p(char *s)
 {
 	char *t;
 
-	if((t = strdup(s)) == nil) {
-		fprint(2, "out of memory in strdup(%.10s)\n", s);
-		exits("mem");
-	}
+	if((t = strdup(s)) == nil)
+		sysfatal("out of memory in strdup(%.20s)", s);
 	setmalloctag(t, getcallerpc(&s));
 	return t;
 }
