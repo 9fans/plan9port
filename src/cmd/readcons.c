ea77b9ce7c579a2e625806dc01104d5f6929cc43
rsc <devnull@localhost>
2005-02-13 18:03:41 +0000

    new program readcons

---
+++ src/cmd/readcons.c
@@ -0,0 +1,39 @@
+#include <u.h>
+#include <libc.h>
+
+void
+usage(void)
+{
+	fprint(2, "usage: readcons [-s] [-d default] prompt\n");
+	exits("usage");
+}
+
+void
+main(int argc, char **argv)
+{
+	char *def, *p;
+	int secret;
+	
+	def = nil;
+	secret = 0;
+	ARGBEGIN{
+	case 's':
+		secret = 1;
+		break;
+	case 'd':
+		def = EARGF(usage());
+		break;
+	default:
+		usage();
+	}ARGEND
+	
+	if(argc != 1)
+		usage();
+
+	p = readcons(argv[0], def, secret);
+	if(p == nil)
+		exits("readcons");
+	print("%s\n", p);
+	exits(0);
+}
+
