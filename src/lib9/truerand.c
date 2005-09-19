3a6c348fba8ca00ff22315a0e270eca5bc55e84d
rsc <devnull@localhost>
2005-09-19 16:40:56 +0000

    OpenBSD lets you open /dev/random but not read it!

--- src/lib9/truerand.c
+++ src/lib9/truerand.c
@@ -7,17 +7,19 @@ truerand(void)
 	int i, n;
 	uchar buf[sizeof(ulong)];
 	static int randfd = -1;
+	static char *randfile;
 
 	if(randfd < 0){
-		randfd = open("/dev/random", OREAD);
-		if(randfd < 0)
-			randfd = open("/dev/srandom", OREAD);	/* OpenBSD */
+		randfd = open(randfile="/dev/random", OREAD);
+		/* OpenBSD lets you open /dev/random but not read it! */
+		if(randfd < 0 || read(randfd, buf, 1) != 1)
+			randfd = open(randfile="/dev/srandom", OREAD);	/* OpenBSD */
 		if(randfd < 0)
 			sysfatal("can't open /dev/random: %r");
 		fcntl(randfd, F_SETFD, FD_CLOEXEC);
 	}
 	for(i=0; i<sizeof(buf); i += n)
 		if((n = readn(randfd, buf+i, sizeof(buf)-i)) < 0)
-			sysfatal("can't read /dev/random: %r");
+			sysfatal("can't read %s: %r", randfile);
 	return *((ulong*)buf);
 }
