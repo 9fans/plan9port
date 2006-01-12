cee7a6ab10235f764435f394d98b9c1e33b4eeb4
rsc <devnull@localhost>
2006-01-12 04:44:25 +0000

    no more biting

--- src/libframe/frdraw.c
+++ src/libframe/frdraw.c
@@ -123,6 +123,9 @@ frtick(Frame *f, Point pt, int ticked)
 		return;
 	pt.x--;	/* looks best just left of where requested */
 	r = Rect(pt.x, pt.y, pt.x+FRTICKW, pt.y+f->font->height);
+	/* can go into left border but not right */
+	if(r.max.x > f->r.max.x)
+		r.max.x = f->r.max.x;
 	if(ticked){
 		draw(f->tickback, f->tickback->r, f->b, nil, pt);
 		draw(f->b, r, f->tick, nil, ZP);
