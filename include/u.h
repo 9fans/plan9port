838874f06406b0c3c57c03176d0c7c7caab937d5
rsc <devnull@localhost>
2006-04-04 11:58:52 +0000

    get strtoll on freebsd

--- include/u.h
+++ include/u.h
@@ -20,6 +20,14 @@ extern "C" {
 #	define _XOPEN_SOURCE 1000
 #	define _XOPEN_SOURCE_EXTENDED 1
 #endif
+#if defined(__FreeBSD__)
+#	include <sys/cdefs.h>
+	/* for strtoll */
+#	undef __ISO_C_VISIBLE
+#	define __ISO_C_VISIBLE 1999
+#	undef __LONG_LONG_SUPPORTED
+#	define __LONG_LONG_SUPPORTED
+#endif
 #define _LARGEFILE64_SOURCE 1
 #define _FILE_OFFSET_BITS 64
 
