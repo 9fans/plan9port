fa07e3d777fc52a7c564f71c9b7f2bba1d0e7716
rsc <devnull@localhost>
2006-04-22 00:50:08 +0000

    16-byte align

--- src/libthread/Darwin-386.c
+++ src/libthread/Darwin-386.c
@@ -3,10 +3,19 @@
 void
 makecontext(ucontext_t *ucp, void (*func)(void), int argc, ...)
 {
+	int n;
 	int *sp;
 
 	sp = (int*)ucp->uc_stack.ss_sp+ucp->uc_stack.ss_size/4;
 	sp -= argc;
+	/*
+	 * Stack pointer at call instruction (before return address
+	 * gets pushed) must be 16-byte aligned.
+	 */
+	if((uintptr)sp%4)
+		abort();
+	while((uintptr)sp%16)
+		sp--;
 	memmove(sp, &argc+1, argc*sizeof(int));
 	*--sp = 0;		/* return address */
 	ucp->uc_mcontext.mc_eip = (long)func;
