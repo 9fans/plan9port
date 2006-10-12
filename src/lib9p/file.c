729e53b9e55756f1b0475195489cf7309a2b80ac
rsc <devnull@localhost>
2006-10-12 01:01:02 +0000

    correct ref counting in walkfile (Nemo)

--- src/lib9p/file.c
+++ src/lib9p/file.c
@@ -232,21 +232,17 @@ File*
 walkfile(File *f, char *path)
 {
 	char *os, *s, *nexts;
-	File *nf;
 
 	if(strchr(path, '/') == nil)
 		return walkfile1(f, path);	/* avoid malloc */
 
 	os = s = estrdup9p(path);
-	incref(&f->ref);
 	for(; *s; s=nexts){
 		if(nexts = strchr(s, '/'))
 			*nexts++ = '\0';
 		else
 			nexts = s+strlen(s);
-		nf = walkfile1(f, s);
-		decref(&f->ref);
-		f = nf;
+		f = walkfile1(f, s);
 		if(f == nil)
 			break;
 	}
