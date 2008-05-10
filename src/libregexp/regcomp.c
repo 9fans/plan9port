002d2f2610604626d672fd45eb79d75059028da3
Russ Cox <rsc@swtch.com>
2008-05-10 13:36:05 -0400

    libregexp: more placating of gcc (John Gosset)

--- src/libregexp/regcomp.c
+++ src/libregexp/regcomp.c
@@ -261,18 +261,18 @@ optimize(Reprog *pp)
 		case STAR:
 		case PLUS:
 		case QUEST:
-			*(char**)(void*)&inst->u1.right += diff;
+			inst->u1.right = (void*)((char*)inst->u1.right + diff);
 			break;
 		case CCLASS:
 		case NCCLASS:
-			*(char**)(void*)&inst->u1.right += diff;
+			inst->u1.right = (void*)((char*)inst->u1.right + diff);
 			cl = inst->u1.cp;
-			*(char**)(void*)&cl->end += diff;
+			cl->end = (void*)((char*)cl->end + diff);
 			break;
 		}
-		*(char**)(void*)&inst->u2.left += diff;
+		inst->u2.left = (void*)((char*)inst->u2.left + diff);
 	}
-	*(char**)(void*)&npp->startinst += diff;
+	npp->startinst = (void*)((char*)npp->startinst + diff);
 	return npp;
 }
 
