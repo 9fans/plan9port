3d36f4437348227c5bad62587dc12b5fd4a3e95e
grai <t.lainson@gmail.com>
2008-07-22 03:21:55 +1000

    devdraw: keep Ctl-Alt from turning into Ctl-U on X11

--- src/cmd/devdraw/x11-itrans.c
+++ src/cmd/devdraw/x11-itrans.c
@@ -127,7 +127,7 @@ __xtoplan9kbd(XEvent *e)
 	if(k == XK_hyphen)
 		k = XK_minus;
 	/* Do control mapping ourselves if translator doesn't */
-	if(e->xkey.state&ControlMask)
+	if(e->xkey.state&ControlMask && k != Kalt)
 		k &= 0x9f;
 	if(k == NoSymbol) {
 		return -1;
