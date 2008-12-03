41305b23558a51c5a6fa33bc7476df1ed6221667
Jeff Sickel <jas@corpus-callosum.com>
2008-12-03 02:13:38 -0600

    libdraw: add borderop

--- src/libdraw/border.c
+++ src/libdraw/border.c
@@ -3,19 +3,25 @@
 #include <draw.h>
 
 void
-border(Image *im, Rectangle r, int i, Image *color, Point sp)
+borderop(Image *im, Rectangle r, int i, Image *color, Point sp, Drawop op)
 {
 	if(i < 0){
 		r = insetrect(r, i);
 		sp = addpt(sp, Pt(i,i));
 		i = -i;
 	}
-	draw(im, Rect(r.min.x, r.min.y, r.max.x, r.min.y+i),
-		color, nil, sp);
-	draw(im, Rect(r.min.x, r.max.y-i, r.max.x, r.max.y),
-		color, nil, Pt(sp.x, sp.y+Dy(r)-i));
-	draw(im, Rect(r.min.x, r.min.y+i, r.min.x+i, r.max.y-i),
-		color, nil, Pt(sp.x, sp.y+i));
-	draw(im, Rect(r.max.x-i, r.min.y+i, r.max.x, r.max.y-i),
-		color, nil, Pt(sp.x+Dx(r)-i, sp.y+i));
+	drawop(im, Rect(r.min.x, r.min.y, r.max.x, r.min.y+i),
+		color, nil, sp, op);
+	drawop(im, Rect(r.min.x, r.max.y-i, r.max.x, r.max.y),
+		color, nil, Pt(sp.x, sp.y+Dy(r)-i), op);
+	drawop(im, Rect(r.min.x, r.min.y+i, r.min.x+i, r.max.y-i),
+		color, nil, Pt(sp.x, sp.y+i), op);
+	drawop(im, Rect(r.max.x-i, r.min.y+i, r.max.x, r.max.y-i),
+		color, nil, Pt(sp.x+Dx(r)-i, sp.y+i), op);
+}
+
+void
+border(Image *im, Rectangle r, int i, Image *color, Point sp)
+{
+	borderop(im, r, i, color, sp, SoverD);
 }
