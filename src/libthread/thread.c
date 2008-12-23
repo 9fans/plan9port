0ac670585c5eac5a6348aeb0e7b018659b2dbc09
Russ Cox <rsc@swtch.com>
2008-12-23 13:03:07 -0800

    libthread: work around gcc warning on FreeBSD

--- src/libthread/thread.c
+++ src/libthread/thread.c
@@ -128,7 +128,7 @@ threadalloc(void (*fn)(void*), void *arg, uint stack)
 
 	/* call makecontext to do the real work. */
 	/* leave a few words open on both ends */
-	t->context.uc.uc_stack.ss_sp = t->stk+8;
+	t->context.uc.uc_stack.ss_sp = (void*)(t->stk+8);
 	t->context.uc.uc_stack.ss_size = t->stksize-64;
 #if defined(__sun__) && !defined(__MAKECONTEXT_V2_SOURCE)		/* sigh */
 	/* can avoid this with __MAKECONTEXT_V2_SOURCE but only on SunOS 5.9 */
