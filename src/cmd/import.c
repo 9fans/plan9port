c77d5b8e1cab6c80b3b677cb0912110086a8fa12
Russ Cox <rsc@swtch.com>
2010-02-22 16:55:42 -0800

    import: use command line service name
    R=rsc
    http://codereview.appspot.com/220042

--- src/cmd/import.c
+++ src/cmd/import.c
@@ -150,7 +150,7 @@ post(char *srv)
 		fatal("can't create pipe: %r");
 
 	/* 0 will be server end, 1 will be client end */
-	if(post9pservice(p[1], "plumb", nil) < 0)
+	if(post9pservice(p[1], srv, nil) < 0)
 		fatal("post9pservice plumb: %r");
 	close(p[1]);
 
