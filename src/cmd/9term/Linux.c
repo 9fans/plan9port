c988c8adf1d997d87ab792f68be0932fed74e9cd
Anthony Martin <ality@pbrane.org>
2010-06-28 14:32:29 -0700

    9term: fix getpts on linux
    Openpty uses grantpt internally and its behavior is
    unspecified when a handler is set to catch SIGCHLD.
    
    Fixes issue 37. http://code.swtch.com/plan9port/issues/37
    
    R=rsc
    CC=codebot
    http://codereview.appspot.com/1209049

--- src/cmd/9term/Linux.c
+++ src/cmd/9term/Linux.c
@@ -1,13 +1,18 @@
 #define getpts not_using_this_getpts
 #include "bsdpty.c"
 #undef getpts
+#include <signal.h>
 
 int
 getpts(int fd[], char *slave)
 {
+	void (*f)(int);
+
+	f = signal(SIGCHLD, SIG_DFL);
 	if(openpty(&fd[1], &fd[0], NULL, NULL, NULL) >= 0){
 		fchmod(fd[1], 0620);
 		strcpy(slave, ttyname(fd[0]));
+		signal(SIGCHLD, f);
 		return 0;
 	}
 	sysfatal("no ptys");
