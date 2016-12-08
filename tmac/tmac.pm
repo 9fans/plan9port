75a4cf00401f1f66079d5cf90288da86a3bfd7be
Xiao-Yong Jin <xjin@anl.gov>
2016-12-08 15:53:42 -0600

    local dir
    Change-Id: I72ec930850c6a4dfa321a9b745f70df0938bfffe

--- tmac/tmac.pm
+++ tmac/tmac.pm
@@ -1,5 +1,5 @@
 .\" 10/22/92 activate next line before installing
-.pi /usr/local/plan9/bin/auxpm
+.pi /Users/jin/pkg/plan9port/bin/auxpm
 .
 .		\" IZ - initialization
 .de IZ
@@ -963,4 +963,4 @@ Naperville, Illinois 60540
 ..
 .IZ
 .rm IZ
-.so /usr/local/plan9/tmac/tmac.srefs
+.so /Users/jin/pkg/plan9port/tmac/tmac.srefs
