afc92cbf8d4658fa56339c0fe42482f8508bee46
Russ Cox <rsc@swtch.com>
2017-07-15 20:23:15 -0600

    acme: implement Cmd-Shift-Z for Redo on Mac
    Change-Id: Ie9332ed473609bd6ca156be0843dc5411cbf7b93

--- src/cmd/acme/text.c
+++ src/cmd/acme/text.c
@@ -757,6 +757,10 @@ texttype(Text *t, Rune r)
 	 	typecommit(t);
 		undo(t, nil, nil, TRUE, 0, nil, 0);
 		return;
+	case Kcmd+'Z':	/* %-shift-Z: redo */
+	 	typecommit(t);
+		undo(t, nil, nil, FALSE, 0, nil, 0);
+		return;		
 
 	Tagdown:
 		/* expand tag to show all text */
