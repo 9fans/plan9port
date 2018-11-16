c63d31a8c1d02e7fa45403a5a10373439d29b250
Russ Cox <rsc@swtch.com>
2018-11-15 22:05:39 -0500

    rio: make 'mk all' explain why it does nothing on non-x11 systems
    Fixes #98.

--- src/cmd/rio/mkriorules.sh
+++ src/cmd/rio/mkriorules.sh
@@ -1,6 +1,8 @@
 if [ "x$WSYSTYPE" != xx11 ]; then
-	echo 'all install clean nuke:Q:'
-	echo '	#'
+	echo 'default:V: all'
+	echo
+	echo 'all install clean nuke:'
+	echo '	# WSYSTYPE is not x11, and rio is only for x11'
 	exit 0
 fi
 cat $PLAN9/src/mkmany
