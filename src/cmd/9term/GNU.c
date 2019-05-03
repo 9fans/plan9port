ba77810ad57562693ee04745983df40f63205b19
Sergey Bugaev <bugaevc@gmail.com>
2019-05-03 19:33:17 +0300

    9term, auxstats: add support for GNU Hurd
    By copying the Linux-specific implementation, since Hurd has a /proc
    filesystem similar to that of Linux. In the future, it may make sense
    to add some Hurd-specific details to it.

---
+++ src/cmd/9term/GNU.c
@@ -0,0 +1,21 @@
+#define getpts not_using_this_getpts
+#include "bsdpty.c"
+#undef getpts
+#include <signal.h>
+#include <pty.h>
+
+int
+getpts(int fd[], char *slave)
+{
+	void (*f)(int);
+
+	f = signal(SIGCHLD, SIG_DFL);
+	if(openpty(&fd[1], &fd[0], NULL, NULL, NULL) >= 0){
+		fchmod(fd[1], 0620);
+		strcpy(slave, ttyname(fd[0]));
+		signal(SIGCHLD, f);
+		return 0;
+	}
+	sysfatal("no ptys");
+	return 0;
+}
