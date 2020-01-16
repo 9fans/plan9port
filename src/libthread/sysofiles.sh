23a23689807560ee80806a339be0f7e7cef6340d
Dan Cross <cross@gajendra.net>
2020-01-16 17:01:44 +0000

    libthread: NetBSD supports pthreads, remove ancient systems in sysofiles.sh
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- src/libthread/sysofiles.sh
+++ src/libthread/sysofiles.sh
@@ -4,15 +4,8 @@ test -f $PLAN9/config && . $PLAN9/config
 
 tag="$OBJTYPE-$SYSNAME-${SYSVERSION:-`uname -r`}-${CC9:-cc}"
 case "$tag" in
-*-Linux-2.[0-5]*)
-	# will have to fix this for linux power pc
-	echo ${SYSNAME}-${OBJTYPE}-asm.o $SYSNAME.o stkmalloc.o
-	;;
-*-FreeBSD-[0-4].*)
-	echo ${SYSNAME}-${OBJTYPE}-asm.o $SYSNAME.o stkmalloc.o
-	;;
 *-NetBSD-*)
-	echo ${SYSNAME}-${OBJTYPE}-asm.o $SYSNAME.o stkmalloc.o
+	echo ${SYSNAME}-${OBJTYPE}-asm.o $SYSNAME.o pthread.o stkmalloc.o
 	;;
 *-Darwin-10.[5-6].* | *-Darwin-[89].*)
 	echo ${SYSNAME}-${OBJTYPE}-asm.o $SYSNAME-${OBJTYPE}.o pthread.o stkmalloc.o
