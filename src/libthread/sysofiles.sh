4698bde2367f971fed3924f2dee736e0dfe37bb9
Russ Cox <rsc@swtch.com>
2020-01-19 23:00:26 -0500

    libthread: clean up sysofiles.sh a bit more

--- src/libthread/sysofiles.sh
+++ src/libthread/sysofiles.sh
@@ -7,37 +7,22 @@ NetBSD)
 	echo ${SYSNAME}-${OBJTYPE}-asm.o $SYSNAME.o stkmalloc.o
 	;;
 OpenBSD)
-	echo ${SYSNAME}-${OBJTYPE}-asm.o pthread.o stkmmap.o
+	echo pthread.o stkmmap.o
 	;;
 *)
 	echo pthread.o stkmalloc.o
 esac
 
 # Various libc don't supply swapcontext, makecontext, so we do.
-case "$OBJTYPE-$SYSNAME" in
-386-OpenBSD)
-	echo 386-ucontext.o
-	;;
-arm-Linux)
-	echo arm-ucontext.o
-	echo Linux-arm-context.o # setcontext, getcontext
-	;;
-arm-NetBSD)
-	echo arm-ucontext.o
-	;;
-power-OpenBSD)
-	echo power-ucontext.o
-	;;
-sparc64-Linux)
-	echo sparc64-ucontext.o
-	echo Linux-sparc64-swapcontext.o # setcontext, getcontext
-	;;
-x86_64-Darwin)
-	echo x86_64-ucontext.o
-	echo Darwin-x86_64-asm.o # setcontext, getcontext
-	;;
-x86_64-OpenBSD)
-	echo x86_64-ucontext.o
+case "$SYSNAME-$OBJTYPE" in
+Darwin-x86_64 | Linux-arm | Linux-sparc64 | NetBSD-arm | OpenBSD-386 | OpenBSD-power | OpenBSD-x86_64)
+	echo $OBJTYPE-ucontext.o
 	;;
 esac
 
+# A few libc don't supply setcontext, getcontext, so we do.
+case "$SYSNAME-$OBJTYPE" in
+Darwin-x86_64 | Linux-arm | Linux-sparc64 | OpenBSD-386 | OpenBSD-power | OpenBSD-x86_64)
+	echo $SYSNAME-$OBJTYPE-asm.o
+	;;
+esac
