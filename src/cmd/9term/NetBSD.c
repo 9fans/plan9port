875d83d18a79ff8786e951a15789fc450206b4b9
Nicola Girardi <nicola@aloc.in>
2020-03-20 18:52:41 +0000

    9term: use openpty on NetBSD
    Fixes #376.

--- src/cmd/9term/NetBSD.c
+++ src/cmd/9term/NetBSD.c
@@ -1,1 +1,17 @@
+#define getpts not_using_this_getpts
 #include "bsdpty.c"
+#undef getpts
+
+#include <util.h>
+
+int
+getpts(int fd[], char *slave)
+{
+	if(openpty(&fd[1], &fd[0], NULL, NULL, NULL) >= 0){
+		fchmod(fd[1], 0620);
+		strcpy(slave, ttyname(fd[0]));
+		return 0;
+	}
+	sysfatal("no ptys: %r");
+	return 0;
+}
