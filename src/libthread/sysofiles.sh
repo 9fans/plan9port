329831171dd6ef81c113f101093c7b4947381003
Xiao-Yong <xjin@anl.gov>
2020-06-15 22:18:03 -0500

    libthread: use libc functions in ucontext for macOS (#417)

--- src/libthread/sysofiles.sh
+++ src/libthread/sysofiles.sh
@@ -15,14 +15,14 @@ esac
 
 # Various libc don't supply swapcontext, makecontext, so we do.
 case "$SYSNAME-$OBJTYPE" in
-Darwin-x86_64 | Linux-arm | Linux-sparc64 | NetBSD-arm | OpenBSD-386 | OpenBSD-power | OpenBSD-x86_64)
+Linux-arm | Linux-sparc64 | NetBSD-arm | OpenBSD-386 | OpenBSD-power | OpenBSD-x86_64)
 	echo $OBJTYPE-ucontext.o
 	;;
 esac
 
 # A few libc don't supply setcontext, getcontext, so we do.
 case "$SYSNAME-$OBJTYPE" in
-Darwin-x86_64 | Linux-arm | Linux-sparc64 | OpenBSD-386 | OpenBSD-power | OpenBSD-x86_64)
+Linux-arm | Linux-sparc64 | OpenBSD-386 | OpenBSD-power | OpenBSD-x86_64)
 	echo $SYSNAME-$OBJTYPE-asm.o
 	;;
 esac
