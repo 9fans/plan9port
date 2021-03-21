a2422a89c5ef6cffd71599aaa0af47423eb5a7f9
Andriy Symonovych <sima.rv@gmail.com>
2021-03-21 23:22:49 +0200

    trim button & modbutton images

--- src/cmd/acme/acme.c
+++ src/cmd/acme/acme.c
@@ -1054,7 +1054,7 @@ iconinit(void)
 		textcols[HTEXT] = display->black;
 	}
 
-	r = Rect(0, 0, Scrollwid+ButtonBorder, font->height+1);
+	r = Rect(0, 0, Scrollwid, font->height+1);
 	if(button && eqrect(r, button->r))
 		return;
 
@@ -1066,13 +1066,11 @@ iconinit(void)
 
 	button = allocimage(display, r, screen->chan, 0, DNofill);
 	draw(button, r, tagcols[BACK], nil, r.min);
-	r.max.x -= ButtonBorder;
 	border(button, r, ButtonBorder, tagcols[BORD], ZP);
 
 	r = button->r;
 	modbutton = allocimage(display, r, screen->chan, 0, DNofill);
 	draw(modbutton, r, tagcols[BACK], nil, r.min);
-	r.max.x -= ButtonBorder;
 	border(modbutton, r, ButtonBorder, tagcols[BORD], ZP);
 	r = insetrect(r, ButtonBorder);
 	tmp = allocimage(display, Rect(0,0,1,1), screen->chan, 1, DMedblue);
