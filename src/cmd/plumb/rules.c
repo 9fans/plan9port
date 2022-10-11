a4d35dc3fde82457d1998c89aec8127914913b61
Igor Burago <igor@igorburago.com>
2022-10-12 02:39:43 +0800

    plumber: use strtoul to parse subexpression match variable substitutions

--- src/cmd/plumb/rules.c
+++ src/cmd/plumb/rules.c
@@ -254,36 +254,21 @@ filename(Exec *e, char *name)
 	return cleanname(buf);
 }
 
-static char*
-subexpmatch(Exec *e, char *s, int *numlen)
-{
-	int n, d, ok;
-	char *t;
-
-	n = 0;
-	ok = 1;
-	for(t = s; '0'<=*t && *t<='9'; t++)
-		if(ok){
-			d = *t-'0';
-			if(d<NMATCHSUBEXP && n<=(NMATCHSUBEXP-1-d)/10)
-				n = 10*n+d;
-			else
-				ok = 0;
-		}
-	*numlen = t-s;
-	if(t==s || !ok)
-		return nil;
-	return e->match[n];
-}
-
 char*
 dollar(Exec *e, char *s, int *namelen)
 {
 	int n;
+	ulong m;
+	char *t;
 	static char *abuf;
 
-	if(e!=nil && '0'<=s[0] && s[0]<='9')
-		return nonnil(subexpmatch(e, s, namelen));
+	if(e!=nil && '0'<=s[0] && s[0]<='9'){
+		m = strtoul(s, &t, 10);
+		*namelen = t-s;
+		if(t==s || m>=NMATCHSUBEXP)
+			return "";
+		return nonnil(e->match[m]);
+	}
 
 	n = scanvarname(s)-s;
 	*namelen = n;
