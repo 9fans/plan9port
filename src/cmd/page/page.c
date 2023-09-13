9fans/plan9port

commit 940f1fd6af2c144d0db087fefa8478d2a36633d5

page: scale ppi on high-res displays
Also fix wording of -w warning.

src/cmd/page/page.c
@@ -96,7 +96,6 @@ threadmain(int argc, char **argv)
 {
 	Document *doc;
 	Biobuf *b;
-	char *basename = argv[0];
 	enum { Ninput = 16 };
 	uchar buf[Ninput+1];
 	int readstdin;
@@ -134,7 +133,7 @@ threadmain(int argc, char **argv)
 		truetoboundingbox = 1;
 		break;
 	case 'w':
-		fprint(2, "%s: -w has only the effect of -R X11 systems\n", basename);
+		fprint(2, "warning: page -w only supported on x11 systems\n");
 		resizing = 1;
 		break;
 	case 'i':
@@ -199,6 +198,13 @@ threadmain(int argc, char **argv)
 	}else
 		b = nil;
 
+	if(initdraw(0, 0, "page") < 0){
+		fprint(2, "page: initdraw failed: %r\n");
+		wexits("initdraw");
+	}
+	display->locking = 1;
+	ppi = scalesize(display, ppi);
+
 	buf[Ninput] = '\0';
 	if(imagemode)
 		doc = initgfx(nil, 0, nil, nil, 0);
@@ -236,12 +242,6 @@ threadmain(int argc, char **argv)
 
 	if(reverse == -1) /* neither cmdline nor ps reader set it */
 		reverse = 0;
-
-	if(initdraw(0, 0, "page") < 0){
-		fprint(2, "page: initdraw failed: %r\n");
-		wexits("initdraw");
-	}
-	display->locking = 1;
 
 	truecolor = screen->depth > 8;
 	viewer(doc);

commit 984c2824e3569479bace65bdaf9e78a2eb36dd58

page: fix viewing of graphics
940f1fd6af broke page on non-PDF files.

src/cmd/page/page.c
@@ -98,8 +98,9 @@ threadmain(int argc, char **argv)
 	Biobuf *b;
 	enum { Ninput = 16 };
 	uchar buf[Ninput+1];
-	int readstdin;
+	int readstdin, haveppi;
 
+	haveppi = 0;
 	ARGBEGIN{
 	/* "temporary" debugging options */
 	case 'P':
@@ -127,6 +128,7 @@ threadmain(int argc, char **argv)
 		reverse = 1;
 		break;
 	case 'p':
+		haveppi = 1;
 		ppi = atoi(EARGF(usage()));
 		break;
 	case 'b':
@@ -223,7 +225,7 @@ threadmain(int argc, char **argv)
 	else if(strncmp((char*)buf, "x T ", 4) == 0)
 		doc = inittroff(b, argc, argv, buf, Ninput);
 	else {
-		if(ppi != 100) {
+		if(haveppi) {
 			fprint(2, "page: you can't specify -p with graphic files\n");
 			wexits("-p and graphics");
 		}