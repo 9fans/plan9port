564d45b5a55adeb48f69fdecb2bf4ed1f909850b
Dan Cross <cross@gajendra.net>
2025-07-22 14:42:03 +0000

    config: add $egrep
    Introduce a new variable set in $PLAN9/config:
    $egrep, which is conditionally set to either
    `egrep` or `grep -E`, based on platform, and use
    this when we want to invoke `egrep`.
    
    This works around the endless warnings from GNU
    grep stating that `egrep` is deprecated in favor
    of `grep -E`, when run on e.g. Linux.

--- dist/checkman.awk
+++ dist/checkman.awk
@@ -446,7 +446,7 @@ END {
 			gsub("\\(", " \\(", b)
 			gsub("\\)", "\\)", b)
 			split(tolower(i), a, "/")
-			print "egrep -in '^\\.IR.*" b "' $PLAN9/man/man*/* # Need " tolower(i) |"sort"
+			print "grep -in '^\\.IR.*" b "' $PLAN9/man/man*/* # Need " tolower(i) |"sort"
 		}
 	}
 	close("sort")
--- src/cmd/devdraw/mkwsysrules.sh
+++ src/cmd/devdraw/mkwsysrules.sh
@@ -24,7 +24,7 @@ fi
 
 if [ "x$WSYSTYPE" = "x" ]; then
 	if [ "x`uname`" = "xDarwin" ]; then
-		if sw_vers | egrep 'ProductVersion:	(10\.[0-9]\.|10\.1[012])$' >/dev/null; then
+		if sw_vers | $egrep 'ProductVersion:	(10\.[0-9]\.|10\.1[012])$' >/dev/null; then
 			echo 1>&2 'OS X 10.12 and older are not supported'
 			exit 1
 		fi
